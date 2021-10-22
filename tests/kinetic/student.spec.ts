9bd222eb58441c355f6ac6d81c11e5adec94f2b2
Greg Fitch <greg.fitch@rice.edu>
2021-10-22 13:17:51 -0500

    remove duplicate test, update npm

--- tests/kinetic/student.spec.ts
+++ tests/kinetic/student.spec.ts
@@ -3,7 +3,7 @@ import { userSignIn } from '../../src/utilities/user'
 import { accountsUserSignup, test } from './helpers'
 
 test('new user sign up @e2e @C639507', async ({ accountsBaseURL, baseURL, kineticBaseURL, page }) => {  // eslint-disable-line
-  // Given: a user viewing the Web landing page for kinetic
+  // Given: a user viewing the Web landing page for Kinetic
   await page.goto('/general/labs')
   // When: they click the 'Sign up' button
   const [newTab] = await Promise.all([page.waitForEvent('popup'), page.click('text=Sign up')])
@@ -12,7 +12,7 @@ test('new user sign up @e2e @C639507', async ({ accountsBaseURL, baseURL, kineti
   // When: they completed the sign up process
   // And:  click the 'Finish' button
   await accountsUserSignup(newTab)
-  // Then: they are taken to the kinetic participant dashboard
+  // Then: they are taken to the Kinetic participant dashboard
   // And:  the demographic survey modal is displayed
   expect(newTab.url()).toBe(`${kineticBaseURL}/studies`)
   const surveyTitle = newTab.locator('.modal-title')
@@ -40,13 +40,13 @@ test('returning user who did not complete the demographic survey see it again @C
 }) => {
   // Setup:
   const student = await accountsUserSignup(page, accountsBaseURL)
-  // Given: a user viewing the kinetic home page
+  // Given: a user viewing the Kinetic home page
   await page.goto(kineticBaseURL)
   // Then: the demographic survey modal is displayed
   const surveyTitle = page.locator('.modal-title')
   expect(surveyTitle).toHaveText('General Demographic Survey')
   // When: they log out
-  // And:  open the kinetic home page
+  // And:  open the Kinetic home page
   // And:  log back in
   await page.goto(accountsBaseURL)
   await page.click('text=Log out')
@@ -62,19 +62,19 @@ test('returning user who completed the demographic survey @C639509', async ({ pa
   // When: they click the "Log in" button
   // Then: the Accounts log in page is displayed in a new tab
   // When: they complete the log in process
-  // Then: they are taken to the kinetic participant dashboard
+  // Then: they are taken to the Kinetic participant dashboard
   // And:  a green completion checkmark is displayed on the survey card
   // And:  the survey card is greyed out and inactive
 })
 
 test('current user selects a survey @C639510', async ({ page }) => {
-  // Given: a logged in user viewing the kinetic participant dashboard
+  // Given: a logged in user viewing the Kinetic participant dashboard
   // When: they select a survey
   // Then: the survey modal is opened
 })
 
 test('current user selects a study @C639511', async ({ page }) => {
-  // Given: a logged in user viewing the kinetic participant dashboard
+  // Given: a logged in user viewing the Kinetic participant dashboard
   // When: they select a study
   // Then: the study details page is displayed
   // When: they click the "Begin study" button
--- tests/kinetic/web.spec.ts
+++ tests/kinetic/web.spec.ts
@@ -12,16 +12,6 @@ test('kinetic landing page is available @C639517', async ({ baseURL, page }) => 
   expect(page.url()).toBe(`${baseURL}/general/kinetic`)
 })
 
-test('users must log in to access kinetic @C639518', async ({ kineticBaseURL, page }) => {
-  // Given: a non-logged in user viewing the Kinetic webpage
-  await page.goto(kineticBaseURL)
-  // Then: the user is asked to log in
-  const header = page.locator('.incorrect-user h1')
-  const link = page.locator('[data-test-id=login-link]')
-  expect(header).toHaveText('Looks like you‘re not  logged in')
-  expect(link).toHaveText('log in')
-})
-
 test('logged in users are taken to kinetic @C640301', async ({ accountsBaseURL, kineticBaseURL, page }) => {
   // Given: a logged in user viewing the Kinetic landing page
   await accountsUserSignup(page, accountsBaseURL)
